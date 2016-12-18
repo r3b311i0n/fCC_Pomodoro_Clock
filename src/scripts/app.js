d915033a8924362ecf7b851bf33bea9a6aca7f40
r3b311i0n <nasashinega@gmail.com>
2016-12-18 15:05:03 +0530

    did some proper stuff.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,48 @@
 require("../styles/styles.css");
+
+class Pomodoro {
+    timer;
+    session;
+    respite;
+
+    constructor() {
+        const sessionPlus = document.getElementById("sessionPlus");
+        const sessionMinus = document.getElementById("sessionMin");
+        const breakPlus = document.getElementById("breakPlus");
+        const breakMinus = document.getElementById("breakMin");
+        this.timer = document.getElementsByClassName("timer");
+        this.session = document.getElementsByClassName("session");
+        this.respite = document.getElementsByClassName("break");
+
+        sessionPlus.addEventListener("click", this.sessionPlus);
+        sessionMinus.addEventListener("click", this.sessionMin);
+        breakPlus.addEventListener("click", this.breakPlus);
+        breakMinus.addEventListener("click", this.breakMin);
+    }
+
+    sessionPlus = () => {
+        let time = parseInt(this.timer[0].innerHTML);
+        this.session[0].innerHTML = time + 1;
+        this.timer[0].innerHTML = time + 1;
+    };
+
+    sessionMin = () => {
+        let time = parseInt(this.timer[0].innerHTML);
+        this.session[0].innerHTML = time - 1;
+        this.timer[0].innerHTML = time - 1;
+    };
+
+    breakPlus = () => {
+        let time = parseInt(this.respite[0].innerHTML);
+        this.respite[0].innerHTML = time + 1;
+    };
+
+    breakMin = () => {
+        let time = parseInt(this.respite[0].innerHTML);
+        this.respite[0].innerHTML = time - 1;
+    };
+}
+
+window.onload = function () {
+    new Pomodoro();
+};
